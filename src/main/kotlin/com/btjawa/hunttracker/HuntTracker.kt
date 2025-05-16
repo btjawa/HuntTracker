b4932c20f3e5285b51fce32d6245e8fb61e21736
btjawa <btj2407@gmail.com>
2025-05-17 00:55:38 +0800

    chore: hello world test

--- src/main/kotlin/com/btjawa/hunttracker/HuntTracker.kt
+++ src/main/kotlin/com/btjawa/hunttracker/HuntTracker.kt
@@ -1,14 +1,31 @@
 package com.btjawa.hunttracker
 
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.NamedTextColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class HuntTracker : JavaPlugin() {
+class HuntTracker : JavaPlugin(), Listener {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.pluginManager.registerEvents(this, this);
+        logger.info("HuntTracker enabled");
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        HandlerList.unregisterAll(this as Listener);
+        logger.info("HuntTracker disabled");
+    }
+
+    @EventHandler
+    fun onPlayerJoin(event: PlayerJoinEvent) {
+        server.broadcast(
+            Component.text("${event.player.name}, Hello World!", NamedTextColor.GREEN)
+        );
     }
 }
