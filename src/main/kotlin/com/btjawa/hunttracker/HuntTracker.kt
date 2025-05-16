af4469399847d3fcf0f543e5197e522af7024189
btjawa <btj2407@gmail.com>
2025-05-17 00:47:34 +0800

    Initial commit

---
+++ src/main/kotlin/com/btjawa/hunttracker/HuntTracker.kt
@@ -0,0 +1,14 @@
+package com.btjawa.hunttracker
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class HuntTracker : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
